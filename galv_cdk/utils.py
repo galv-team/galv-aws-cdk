d1104299b0e1abe34fb5137122cb0c524dd0965a
Matt Jaquiery <matt.jaquiery@dtc.ox.ac.uk>
2025-06-20 08:13:38 +0100

    fix: WAF action syntax again

--- galv_cdk/utils.py
+++ galv_cdk/utils.py
@@ -90,14 +90,14 @@ def create_waf_scope_web_acl(scope, id, *, name: str, scope_type: str, log_bucke
                 statement=wafv2.CfnWebACL.StatementProperty(
                     managed_rule_group_statement=wafv2.CfnWebACL.ManagedRuleGroupStatementProperty(
                         name="AWSManagedRulesCommonRuleSet",
-                        vendor_name="AWS"
+                        vendor_name="AWS",
+                        rule_action_overrides=[
+                            wafv2.CfnWebACL.RuleActionOverrideProperty(
+                                name="SizeRestrictions_BODY",
+                                action_to_use=wafv2.CfnWebACL.RuleActionProperty(count={}),
+                            )
+                        ],
                     ),
-                    rule_action_overrides=[
-                        wafv2.CfnWebACL.RuleActionOverrideProperty(
-                            name="SizeRestrictions_BODY",
-                            action_to_use=wafv2.CfnWebACL.RuleActionProperty(count={}),
-                        )
-                    ],
                 ),
                 visibility_config=wafv2.CfnWebACL.VisibilityConfigProperty(
                     sampled_requests_enabled=True,
